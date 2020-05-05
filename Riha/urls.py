f74ab72f118e26e8cf489b16a52c7498dae846d4
Desos_Wajos <55079306+Desire1990@users.noreply.github.com>
2020-05-05 16:33:25 +0200

    Update urls.py

--- Riha/urls.py
+++ Riha/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from settings import DEBUG, STATIC_URL, STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('idcomplete/',include('idcomplete.urls')),
     path('', include('account.urls')),
 ]
+
+if DEBUG:
+	urlpatterns += static(STATIC_URL, document_root = STATIC_ROOT)
+	urlpatterns += static(MEDIA_URL, document_root = MEDIA_ROOT)
